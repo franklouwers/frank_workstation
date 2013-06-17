599fdceeef4f873afe11912e96ab8f828207298f
Frank Louwers <frank@openminds.be>
2013-06-17 14:43:12 +0200

    WS_USER legacy

--- recipes/openoffice.rb
+++ recipes/openoffice.rb
@@ -2,6 +2,6 @@ dmg_package "OpenOffice.org" do
   volumes_dir "OpenOffice.org"
   source "http://downloads.sourceforge.net/project/openofficeorg.mirror/stable/3.4.1/Apache_OpenOffice_incubating_3.4.1_MacOS_x86_install_en-US.dmg"
   checksum "4e9fcad97e83897f3bafce80593b88fa55559901880ade65cf0cbfc77c0a719b"
-  owner WS_USER
+  owner node['current_user']
   action :install
 end
